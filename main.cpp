c2311fb32b42b29ec736dd2f15ab901617f689b8
Ian Carlos Azevedo Martins <iancarlos335@gmail.com>
2024-03-15 09:14:23 -0300

    Atualizando o código com a separacao

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bitset>
 #include <cstdlib>
+#include <algorithm>
 #include <cmath>
 #include <ctime>
 using namespace std;
@@ -16,7 +17,10 @@ void gerarIndividuos(int individuos[maxIndividuos], int &a, int &b, int &c, int 
         cout << "Seja bem vindo ao sistema de genética" << endl
              << "Por favor, digite quantas populações gostaria de gerar (10, 100 ou 1000): " << endl;
         cin >> quantidadeIndividuos;
-        if (quantidadeIndividuos != (1000 || 100 || 10))
+
+        if ((quantidadeIndividuos != 1000) &&
+            (quantidadeIndividuos != 100) &&
+            (quantidadeIndividuos != 10))
         {
             cout << "Por favor, escolha uma das opções válidas" << endl;
             continue;
@@ -58,7 +62,7 @@ void gerarIndividuos(int individuos[maxIndividuos], int &a, int &b, int &c, int 
         // Gera os individuos aletóriamente dentro do range de -2000 a 2000
         for (int i = 0; i < quantidadeIndividuos; i++)
         {
-            individuos[i] = rand() % 4001 - 2000;
+            individuos[i] = rand() % 5;
         }
         break;
     }
@@ -86,33 +90,56 @@ bitset<32> passibilidadeDeMutacao(bitset<32> conteudoBit)
     return conteudoBit;
 }
 
+void separacao(int individuos[maxIndividuos])
+{
+    int taxaDeSeparação = quantidadeIndividuos * 0.5 + 1;
+    for (int i = taxaDeSeparação; i < quantidadeIndividuos; i++)
+    {
+        individuos[i] = rand() % 5;
+    }
+    cout << endl;
+}
+
+void validaSolucaoBoa(int individuos[maxIndividuos], int a, int b, int c, int d, int e, int f, int x, long resultado)
+{
+    for (int geracaoAtual = 0; geracaoAtual < numeroDeGeracoes; geracaoAtual++)
+    {
+        for (int individuoAtual = 0; individuoAtual < quantidadeIndividuos; individuoAtual++)
+        {
+            x = individuos[individuoAtual];
+            cout << a * pow(x, 5);
+            resultado = (a * pow(x, 5)) + (b * pow(x, 4)) + (c * pow(x, 3)) + (d * pow(x, 2)) + (e * x) + f;
+            if (resultado == 0)
+            {
+                cout << "Resultado da equação: " << x << endl
+                     << "Com o indivíduo: " << individuoAtual << endl
+                     << "Na geração" << geracaoAtual + 1 << endl;
+                // exit(EXIT_FAILURE);
+            }
+            else
+            {
+                separacao(individuos);
+            }
+        }
+    }
+}
+
 int main()
 {
     srand(time(0));
-    int a, b, c, d, e, f, x, resultado;
+    int a, b, c, d, e, f, x;
+    long resultado;
     int individuos[maxIndividuos]; // Perguntar pro professor se tem problema colocar esse vetor num contexto global.
 
     gerarIndividuos(individuos, a, b, c, d, e, f);
+    sort(individuos[0], individuos[quantidadeIndividuos - 1]); // Ordena os individuos em ordem crescente
+
+    validaSolucaoBoa(individuos, a, b, c, d, e, f, x, resultado);
 
     for (int i = 0; i < quantidadeIndividuos; i++)
     {
         passibilidadeDeMutacao(individuos[i]);
     }
 
-    for (int i = 0; i < numeroDeGeracoes; i++)
-    {
-        for (int j = 0; j < quantidadeIndividuos; j++)
-        {
-            x = individuos[j];
-            resultado = a * pow(x, 5) + b * pow(x, 4) + c * (x, 3) + d * pow(x, 2) + e * x + f;
-            if (resultado == 0)
-            {
-                cout << "Resultado da equação: " << x << endl
-                     << "Para o indivíduo: " << j << endl
-                     << "Na geração" << i << endl;
-                break;
-            }
-        }
-    }
     return 0;
 }
